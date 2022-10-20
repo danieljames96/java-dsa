b0948da06933c578871af3b09de6ade45ccad4b4
danieljames96 <danieljames96@gmail.com>
2022-10-20 10:58:45 +0530

    Commit changes

---
+++ src/main/java/com/learning/WorkingWithStacks.java
@@ -0,0 +1,16 @@
+package com.learning;
+
+import java.util.Stack;
+
+public class WorkingWithStacks {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(4);
+        System.out.println(stack.peek());
+        System.out.println(stack.size());
+        System.out.println(stack.pop());
+        System.out.println(stack.size());
+    }
+}
